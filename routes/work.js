5fa4a74c7af1459d3cf0f26817fa4f16ee9bffb1
Ashish Thakur <sendashishthakur@gmail.com>
2021-06-18 01:27:41 +0530

    using findById

--- routes/work.js
+++ routes/work.js
@@ -73,7 +73,7 @@ router.get('/allworks',(req,res)=>{
 
 
 router.get('/mywork/:id',(req,res)=>{
-    User.findOne({_id:req.params.id}).then((mywork)=>{
+    User.findById({_id:req.params.id}).then((mywork)=>{
         res.send(mywork)
     })
 })
